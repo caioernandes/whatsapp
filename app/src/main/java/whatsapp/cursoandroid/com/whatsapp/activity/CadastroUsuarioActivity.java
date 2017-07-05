e2d08ba6458aca6ffc230847113351143e74ebae
Caio Ernandes Martins do Nascimento <caio-ernandes@hotmail.com>
2017-07-05 01:38:55 -0300

    Criada tela de login e cadastro

--- app/src/main/java/whatsapp/cursoandroid/com/whatsapp/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/whatsapp/cursoandroid/com/whatsapp/activity/CadastroUsuarioActivity.java
@@ -1,22 +1,15 @@
 package whatsapp.cursoandroid.com.whatsapp.activity;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.google.firebase.database.DatabaseReference;
+import android.os.Bundle;
 
 import whatsapp.cursoandroid.com.whatsapp.R;
-import whatsapp.cursoandroid.com.whatsapp.config.ConfiguracaoFirebase;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class CadastroUsuarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_cadastro_usuario);
     }
 }
---
+++ app/src/main/java/whatsapp/cursoandroid/com/whatsapp/config/ConfiguracaoFirebase.java
@@ -0,0 +1,19 @@
+package whatsapp.cursoandroid.com.whatsapp.config;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public final class ConfiguracaoFirebase {
+
+    private static DatabaseReference referenciaFirebase;
+
+    public static DatabaseReference getFirebase() {
+
+        if (referenciaFirebase == null) {
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+
+        return referenciaFirebase;
+    }
+}
